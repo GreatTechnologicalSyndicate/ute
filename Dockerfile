b50a1a0444f6043b9d6ddd966a4cbc1be1263e34
vezono <vezono.lab@gmail.com>
2023-02-26 15:48:07 -0500

    wrapping up

--- Dockerfile
+++ Dockerfile
@@ -6,5 +6,6 @@ COPY requirements.txt requirements.txt
 RUN pip3 install -r requirements.txt
 
 COPY bot db startup main.py .
+COPY config.py .
 
 CMD [ "python3", "main.py" ]
---
+++ config.example.py
@@ -0,0 +1,8 @@
+# Save your config in config.py
+
+mongo_url = 'mongodb://127.0.0.1:27017/?directConnection=true' # dont change unless you know what are you doing
+
+telegram_token = 'bot token'
+owners = [666, 777, 888] # admin user ids
+log_channel = -1007777777 # admin chat id
+
